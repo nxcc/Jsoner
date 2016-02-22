76a48a810ea81579211c13e2c0417b3ddcd18864
Jonas Gröger <jonas.groeger@noris.de>
2016-02-22 14:50:11 +0100

    Add filter / prefix configuration

--- JSONer.hooks.php
+++ JSONer.hooks.php
@@ -7,17 +7,84 @@
  */
 class JSONerHooks
 {
-    public static function onParserSetup( &$parser )
+    private static $configPrefix = 'jsoner';
+
+    public static function onParserSetup(&$parser)
     {
-        $parser->setFunctionHook('what', 'JSONerHooks::what');
+        $parser->setFunctionHook('jsoner', 'JSONerHooks::run');
+
+        MWDebug::log("JSONer: Registering parser extension");
 
         return true; // Always return true, in order not to stop MW's hook processing!
     }
 
-    public static function what( &$parser )
+    /**
+     * Provides a callback for configuration in extension.json
+     * @return GlobalVarConfig The configuration for the JSONer extension
+     */
+    public static function buildConfig()
     {
-        global $wgJSONerBaseUrl;
-        $val = "Function 'what' ($wgJSONerBaseUrl) called.";
-        return $val;
+        return new GlobalVarConfig(self::$configPrefix);
+    }
+
+    public static function run(&$parser)
+    {
+        $config = self::getConfig();
+
+        // TODO: Add i18n
+        if (!self::curlIsInstalled()) {
+            return "<span style='color: red;'>PHP extension cURL not installed.</span>";
+        }
+
+        $url = "https://intra.office.noris.de/rest/crud/person/3050";
+
+        $opts = array();
+        // Argument 0 is $parser, so begin iterating at 1
+        for ($i = 1; $i < func_num_args(); $i++) {
+            $opts[] = func_get_arg($i);
+        }
+        $options = self::extractOptions($opts);
+
+        $jsoner = new jsoner\JSONer($config, $options);
+        return $jsoner->run();
+    }
+
+    private static function getConfig()
+    {
+        $config = ConfigFactory::getDefaultInstance()->makeConfig(self::$configPrefix);
+        MWDebug::log("JSONer: Loaded configuration -> " . print_r($config, true));
+        return $config;
+    }
+
+    private static function curlIsInstalled()
+    {
+        return function_exists('curl_version');
+    }
+
+    /**
+     * Converts an array of values in form [0] => "name=value" into a real
+     * associative array in form [name] => value
+     *
+     * @link https://www.mediawiki.org/wiki/Manual:Parser_functions/de#Named_parameters
+     * @param array $options
+     * @return array $results
+     */
+    private static function extractOptions(array $options)
+    {
+        $results = array();
+
+        foreach ($options as $option) {
+            $pair = explode('=', $option, 2);
+            if (count($pair) == 2) {
+                $name = trim($pair[0]);
+                $value = trim($pair[1]);
+                $results[$name] = $value;
+            }
+        }
+        //Now you've got an array that looks like this:
+        //	[foo] => bar
+        //	[apple] => orange
+
+        return $results;
     }
 }
