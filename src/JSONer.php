8c862d2f46a3da090f3f17c5b571c4b860d8941a
Jonas Gröger <jonas.groeger@noris.de>
2016-02-23 18:26:03 +0100

    Remove debug data and use new Config class
    Also added ElementOrder in the config. Not
    sure where to take it from still. Lets keep it
    at 'id' for now.

--- src/JSONer.php
+++ src/JSONer.php
@@ -7,17 +7,29 @@ use jsoner\transformer\WikitextTransformer;
 
 class JSONer
 {
+	/**
+	 * @var \jsoner\Config The configuration for JSONer
+	 */
 	private $config;
+
+	/**
+	 * @var array User provided options in the #jsoner call
+	 */
 	private $options;
 
 	/**
 	 * JSONer constructor.
-	 * @param \GlobalVarConfig $config Configuration for JSOner
+	 * @param \Config $mwConfig Configuration for JSOner in a MediaWiki datastructure.
 	 * @param $options
 	 */
-	public function __construct( $config, $options ) {
+	public function __construct( $mwConfig, $options ) {
 
-		$this->config = $config;
+		$this->config = new Config( [
+				"BaseUrl" => $mwConfig->get( "BaseUrl" ),
+				"User" => $mwConfig->get( "User" ),
+				"Pass" => $mwConfig->get( "Pass" ),
+				"ElementOrder" => ["id"], // TODO: Make configurable in $options? or $mwConfig?
+		] );
 		$this->options = $options;
 	}
 
@@ -27,13 +39,13 @@ class JSONer
 	 * @throws \ConfigException
 	 */
 	public function run() {
-
-		$url = self::buildUrl( $this->config, $this->options );
+		$queryUrl = self::buildUrl( $this->config['BaseUrl'], $this->options['url'] );
+		$this->config->setItem( "QueryUrl", $queryUrl );
 
 		try {
 			// Resolve
 			$resolver = new Resolver( $this->config );
-			$json = $resolver->resolve( $url );
+			$json = $resolver->resolve( $queryUrl );
 
 			// Parse
 			$json = Parser::parse( $json );
@@ -52,8 +64,10 @@ class JSONer
 			// $transformerRegistry = new TransformerRegistry($this->options);
 			// $transformerRegistry->registerTransformersFromFromNamespace("\\jsoner\\transformer\\");
 
+			$json = self::orderJson( $json, $this->config );
+
 			// Transform
-			$transformer = new WikitextTransformer( $url );
+			$transformer = new WikitextTransformer( $this->config );
 			return $transformer->transform( $json );
 
 		} catch ( CurlException $ce ) { // TODO: NoSuchFilterException, NoSuchTransformerException
@@ -63,15 +77,10 @@ class JSONer
 		}
 	}
 
-	/**
-	 * @param \GlobalVarConfig $config Configuration for JSOner
-	 * @param array $options Provided by the user in a query
-	 * @return string The url to query
-	 */
-	private static function buildUrl( $config, $options ) {
+	private static function buildUrl( $baseUrl, $queryUrl ) {
 
-		$baseUrl = rtrim( $config->get( 'BaseUrl' ), '/' );
-		$queryUrl = trim( $options['url'], '/' );
+		$baseUrl = rtrim( $baseUrl, '/' );
+		$queryUrl = trim( $queryUrl, '/' );
 		return "$baseUrl/$queryUrl/";
 	}
 
@@ -87,12 +96,14 @@ class JSONer
 		foreach ( $options as $filterTag => $filterParams ) {
 
 			// Unknown filter
-			if ( !array_key_exists( $filterTag, $filterMap ) ) { continue;
-	  }
+			if ( !array_key_exists( $filterTag, $filterMap ) ) {
+				continue;
+			}
 
 			// Empty filter args
-			if ( empty( trim( $filterParams ) ) ) { continue;
-	  }
+			if ( empty( trim( $filterParams ) ) ) {
+				continue;
+			}
 
 			$filterName = $filterMap[$filterTag][0];
 			$filterArgc = $filterMap[$filterTag][1];
@@ -133,6 +144,22 @@ class JSONer
 
 			$json = call_user_func( $function, $json, $parameter_array );
 		}
+		return $json;
+	}
+
+	/**
+	 * @param $json
+	 * @param \jsoner\Config $config
+	 * @return array An ordered array according to the configuration
+	 */
+	private static function orderJson( $json, $config ) {
+
+		$ordering = $config->getItem( "ElementOrder" );
+
+		foreach ( $json as $key => $value ) {
+			$json[$key] = array_merge( array_flip( $ordering ), $value );
+		}
+
 		return $json;
 	}
 }
--- src/Resolver.php
+++ src/Resolver.php
@@ -8,106 +8,17 @@ class Resolver
 
 	/**
 	 * Resolver constructor.
-	 * @param \GlobalVarConfig $config
+	 * @param \jsoner\Config $config
 	 */
 	public function __construct( $config ) {
 		$this->config = $config;
 	}
 
-	private $DEBUG_STR = <<<END
-
-{
-  "_data": [
-  {
-    "_title": "NORIS-HOUSING-RZ2",
-    "begin": "2005-10-11 00:00:00",
-    "customer": {
-      "_target": "customer",
-      "_title": "POP",
-      "_url": "http://localhost/crud/customer/1",
-      "id": 1
-    },
-    "end": "2016-02-17 14:30:35",
-    "id": 3,
-    "info": "Housing-Kunden im RZ2",
-    "name": "NORIS-HOUSING-RZ2",
-    "timestamp": "2016-02-17 14:30:37",
-    "vlan_id": 2,
-    "vlancontainer": null,
-    "vlanzone": {
-      "_target": "vlanzone",
-      "_title": "global",
-      "_url": "http://localhost/crud/vlanzone/1",
-      "id": 1
-    }
-  },
-  {
-    "_title": "FHOME-DMZ",
-    "begin": "2007-03-11 00:00:00",
-    "customer": {
-      "_target": "customer",
-      "_title": "fsippel",
-      "_url": "http://localhost/crud/customer/1650",
-      "id": 1650
-    },
-    "end": null,
-    "id": 4,
-    "info": null,
-    "name": "FHOME-DMZ",
-    "timestamp": "2014-03-28 08:31:52",
-    "vlan_id": 3,
-    "vlancontainer": {
-      "_target": "vlancontainer",
-      "_title": "FHOME-DMZ",
-      "_url": "http://localhost/crud/vlancontainer/967",
-      "id": 967
-    },
-    "vlanzone": {
-      "_target": "vlanzone",
-      "_title": "global",
-      "_url": "http://localhost/crud/vlanzone/1",
-      "id": 1
-    }
-  },
-  {
-    "_title": "FHOME-ROUTER-NET",
-    "begin": "2005-10-11 00:00:00",
-    "customer": {
-      "_target": "customer",
-      "_title": "fsippel",
-      "_url": "http://localhost/crud/customer/1650",
-      "id": 1650
-    },
-    "end": null,
-    "id": 5,
-    "info": null,
-    "name": "FHOME-ROUTER-NET",
-    "timestamp": "2015-04-01 15:42:40",
-    "vlan_id": 4,
-    "vlancontainer": {
-      "_target": "vlancontainer",
-      "_title": "FHOME-ROUTER",
-      "_url": "http://localhost/crud/vlancontainer/1592",
-      "id": 1592
-    },
-    "vlanzone": {
-      "_target": "vlanzone",
-      "_title": "global",
-      "_url": "http://localhost/crud/vlanzone/1",
-      "id": 1
-    }
-  }
-]
-}
-END;
-
-
 	public function resolve( $url ) {
-		return $this->DEBUG_STR;
 		$ch = curl_init();
 		try {
-			$user = $this->config->get( "User" );
-			$pass = $this->config->get( "Pass" );
+			$user = $this->config->getItem( "User" );
+			$pass = $this->config->getItem( "Pass" );
 			curl_setopt( $ch, CURLOPT_USERPWD, "$user:$pass" );
 		} catch ( \ConfigException $ex ) {
 			wfDebugLog( 'JSONer', 'Either $jsonerUser or $jsonerPass were not set.'
