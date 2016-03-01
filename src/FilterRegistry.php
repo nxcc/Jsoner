ee13224ed7ddd1d76e88424ed22126379aedf712
Jonas Gröger <jonas.groeger@noris.de>
2016-03-01 14:08:06 +0100

    Add FilterRegistry (not used yet)

--- src/FilterRegistry.php
+++ src/FilterRegistry.php
@@ -2,12 +2,49 @@
 
 namespace jsoner;
 
+use jsoner\exceptions\FilterException;
+use jsoner\filter\Filter;
+
 class FilterRegistry
 {
-	private $filters;
+	private $config;
 
-	public function __construct( $filters ) {
+	private $filters = [
+		'f-SelectKeys' =>    "\\jsoner\\filter\\JsonDumpTransformer",
+		'f-SelectSubtree' => "\\jsoner\\filter\\SingleElementTransformer",
+		'f-RemoveKeys' =>    "\\jsoner\\filter\\WikitextTableTransformer",
+		'f-CensorKeys' =>    "\\jsoner\\filter\\CensorKeysFilter",
+	];
 
-		$this->filters = $filters;
+	public function __construct( $config ) {
+		$this->config = $config;
+	}
+
+	public function addFilter( $key, $fqcn ) {
+		$this->filters[$key] = $fqcn;
+	}
+
+	/**
+	 * @param $key
+	 * @return Filter
+	 * @throws FilterException
+	 */
+	public function getFilterByKey( $key ) {
+		$filterKey = Helper::getArrayValueOrDefault( $this->filters, $key );
+
+		if ( $filterKey !== null ) {
+			return new $filterKey( $this->config );
+		}
+
+		throw new FilterException( "No such filter: '$key'." );
+	}
+
+	public function getFiltersByKeys($filterKeys)
+	{
+		$filters = [];
+		foreach ($filterKeys as $filter) {
+			$filters[] = $this->getFilterByKey($filter);
+		}
+		return $filters;
 	}
 }
