235c6cf7bedf819f877649039121cff983b5d8db
Jonas Gröger <jonas.groeger@noris.de>
2016-03-01 15:30:04 +0100

    Fix bug where the JsonTransformer would not output anything

--- src/transformer/JsonDumpTransformer.php
+++ src/transformer/JsonDumpTransformer.php
@@ -5,18 +5,15 @@ namespace jsoner\transformer;
 class JsonDumpTransformer extends AbstractTransformer
 {
 	public function transformZero( $options ) {
-
 		$emptyJsonObject = "{}";
 		$this->transformMultiple( $emptyJsonObject, $options );
 	}
 
 	public function transformOne( $json , $options ) {
-
-		$this->transformMultiple( $json, $options );
+		return $this->transformMultiple( $json, $options );
 	}
 
 	public function transformMultiple( $json , $options ) {
-
 		$json_encode_options = JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE;
 		return "<pre>" . json_encode( $json, $json_encode_options ) . "</pre>";
 	}
