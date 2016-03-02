ffea0584c138294a6d4de338142106926c49cc47
Jonas Gröger <jonas.groeger@noris.de>
2016-03-02 13:45:06 +0100

    Helper function for array assertion

--- src/filter/FilterHelper.php
+++ src/filter/FilterHelper.php
@@ -5,5 +5,10 @@ namespace jsoner\filter;
 
 class FilterHelper
 {
+	public static function assertIsArrayOrThrow( $maybeArray, $exception = \Exception::class ) {
 
+		if ( !is_array( $maybeArray ) ) {
+			throw new $exception( "Value was not an array!" );
+		}
+	}
 }
